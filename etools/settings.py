9c3cec871c0075cea79f86152bf9dbb7bafd45a0
Vishal Patel <vpatel@sdipresence.com>
2024-02-01 14:53:14 -0600

    look for all dirs

--- etools/settings.py
+++ etools/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'etools.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR],  # Adjusted location for 'DIRS'
+        'DIRS': [],  # Adjusted location for 'DIRS'
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
