178389298304c22935974f14f98781671df0db7e
Vishal Patel <vpatel@sdipresence.com>
2024-02-01 15:37:02 -0600

    inline js and css since djanog is dumb

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,4 @@
-from django.contrib import admin
-from django.urls import path, include  # ensure include is imported
-from django.conf import settings
-from django.conf.urls.static import static
+from django.urls import path
 from .views import home, send_email_view, get_template_content
 
 
@@ -10,7 +7,3 @@ urlpatterns = [
     path('send_email/', send_email_view, name='send_email'),
     path('get_template_content/<int:template_id>/', get_template_content, name='get_template_content'),
 ]
-
-
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
