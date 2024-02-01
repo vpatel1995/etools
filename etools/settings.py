e69d5cc85de317ce28a72bf988d15946a391a191
Vishal Patel <vpatel@sdipresence.com>
2024-02-01 14:37:00 -0600

    try to modify templating

--- etools/settings.py
+++ etools/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'etools.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'etools/templates/etools/'],  # Correct location for 'DIRS'
+        'DIRS': [],  # Adjusted location for 'DIRS'
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
