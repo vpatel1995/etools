2acc45189b598f76c8cb273c1a8bd8b35959f01f
Vishal Patel <vpatel@sdipresence.com>
2024-02-01 15:08:23 -0600

    properluy serve css

--- etools/settings.py
+++ etools/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'home/static/home'),
+    os.path.join(BASE_DIR, 'home', 'static'),
 ]
 
 # Default primary key field type
