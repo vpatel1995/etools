1afd9d5aa240caaf71cb5a8fda6780d487ea42a9
Vishal Patel <vpatel@sdipresence.com>
2024-02-01 12:17:04 -0600

    add in to, cc, and other stuff

--- home/views.py
+++ home/views.py
@@ -19,8 +19,8 @@ def get_template_content(request, template_id):
         return JsonResponse({'error': 'Template not found'}, status=404)
 
 def load_email_template(template_id):
-    template_path = f"email_templates/template{template_id}.txt"
-    with open(template_path, 'r') as file:
+    file_path = os.path.join(settings.BASE_DIR, 'home', 'email_templates', f'template{template_id}.txt')
+    with open(file_path, 'r') as file:
         return file.read()
 
 def home(request):
@@ -28,21 +28,22 @@ def home(request):
 
 def send_email_view(request):
     if request.method == 'POST':
-        # Parse the received JSON data
         data = json.loads(request.body)
         template_id = data.get('template_id')
         template_content = load_email_template(template_id)
-
+        to_email = data.get('to_email')
+        from_email = data.get('from_email')
+        cc_email = data.get('cc_email')
+        subject = data.get('subject')
+        body_html = "<html><head></head><body>" + template_content + "</body></html>"
 
         # AWS SES Configuration
         aws_access_key_id = 'AKIASD7G5KU2SHEC4B52'
         aws_secret_access_key = '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'
         aws_region = "us-east-2"
 
-        sender = "patelv2895@gmail.com"
-        recipient = "patelv2895@gmail.com"
-        subject = data.get('subject')
-        body_html = "<html><head></head><body>" + template_content + "</body></html>"
+        # Use the provided 'from_email' as the sender
+        sender = from_email
 
         # Create a new SES client
         client = boto3.client(
@@ -52,12 +53,15 @@ def send_email_view(request):
             region_name=aws_region
         )
 
+        # Prepare the destination dictionary
+        destination = {'ToAddresses': [to_email]}
+        if cc_email:
+            destination['CcAddresses'] = [cc_email]
+
         # Try to send the email.
         try:
             response = client.send_email(
-                Destination={
-                    'ToAddresses': [recipient],
-                },
+                Destination=destination,
                 Message={
                     'Body': {
                         'Html': {
@@ -78,3 +82,4 @@ def send_email_view(request):
             return JsonResponse({'message': 'Email sent successfully! Message ID: {}'.format(response['MessageId'])}, status=200)
 
     return JsonResponse({'message': 'Invalid request'}, status=400)
+
