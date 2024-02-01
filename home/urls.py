a5c9dd65bd433ced6cbf7a6f11345bcab5937fa7
Vishal Patel <vpatel@sdipresence.com>
2024-02-01 15:27:03 -0600

    adding static to urls

--- home/urls.py
+++ home/urls.py
@@ -1,4 +1,7 @@
-from django.urls import path
+from django.contrib import admin
+from django.urls import path, include  # ensure include is imported
+from django.conf import settings
+from django.conf.urls.static import static
 from .views import home, send_email_view, get_template_content
 
 
@@ -7,3 +10,7 @@ urlpatterns = [
     path('send_email/', send_email_view, name='send_email'),
     path('get_template_content/<int:template_id>/', get_template_content, name='get_template_content'),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
