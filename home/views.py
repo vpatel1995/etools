0bf82579ebfae10c31b87b05f39929fe5536cbbf
Vishal Patel <vpatel@sdipresence.com>
2024-02-01 00:18:41 -0600

    select and display email template

--- home/views.py
+++ home/views.py
@@ -2,30 +2,27 @@ from django.shortcuts import render
 from django.http import JsonResponse
 import boto3
 from botocore.exceptions import ClientError
+import json
 
 def home(request):
     return render(request, 'home/home.html')
 
 def send_email_view(request):
     if request.method == 'POST':
+        # Parse the received JSON data
+        data = json.loads(request.body)
+        template_id = data.get('template_id')
+        template_content = data.get('template_content')
+
         # AWS SES Configuration
         aws_access_key_id = 'AKIASD7G5KU2SHEC4B52'
         aws_secret_access_key = 'asxLtCguymmZJPTcX+kamBhPLCEhXkrFEbLVDhOS'
-        aws_region = "us-east-2"  # Change as per your AWS SES configuration
+        aws_region = "us-east-2"
 
-        sender = "patelv2895@gmail.com"  # Your verified sender address
-        recipient = "patelv2895@gmail.com"  # Your verified recipient address
-        subject = "Amazon SES Test (SDK for Python)"
-        body_text = ("Amazon SES Test\r\n"
-                     "This email was sent with Amazon SES using the AWS SDK for Python (Boto).")
-        body_html = """<html>
-        <head></head>
-        <body>
-          <h1>Amazon SES Test (SDK for Python)</h1>
-          <p>This email was sent with <a href='https://aws.amazon.com/ses/'>Amazon SES</a> using the AWS SDK for Python (Boto).</p>
-        </body>
-        </html>
-        """
+        sender = "patelv2895@gmail.com"
+        recipient = "patelv2895@gmail.com"
+        subject = "Email Template " + template_id
+        body_html = "<html><head></head><body>" + template_content + "</body></html>"
 
         # Create a new SES client
         client = boto3.client(
@@ -47,10 +44,6 @@ def send_email_view(request):
                             'Charset': "UTF-8",
                             'Data': body_html,
                         },
-                        'Text': {
-                            'Charset': "UTF-8",
-                            'Data': body_text,
-                        },
                     },
                     'Subject': {
                         'Charset': "UTF-8",
@@ -59,11 +52,9 @@ def send_email_view(request):
                 },
                 Source=sender,
             )
-        # Display an error if something goes wrong.
         except ClientError as e:
             return JsonResponse({'message': e.response['Error']['Message']}, status=500)
         else:
             return JsonResponse({'message': 'Email sent successfully! Message ID: {}'.format(response['MessageId'])}, status=200)
 
     return JsonResponse({'message': 'Invalid request'}, status=400)
-
